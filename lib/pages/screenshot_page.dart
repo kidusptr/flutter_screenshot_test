51fc8ef45d19b76ddd46c13c54e5eeab6cb83f3b
Kidus Petros <kidusptr@gmail.com>
2024-10-30 16:10:34 +0300

    Removed unused packages and null checks

--- lib/pages/screenshot_page.dart
+++ lib/pages/screenshot_page.dart
@@ -2,7 +2,6 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:screenshot_test/pages/long_widget.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class ScreenShotPage extends StatefulWidget {
   const ScreenShotPage({super.key});
@@ -12,39 +11,32 @@ class ScreenShotPage extends StatefulWidget {
 }
 
 class _ScreenShotPageState extends State<ScreenShotPage> {
-  late final WebViewController _webViewController;
   ScreenshotController screenshotController = ScreenshotController();
 
   @override
-  void initState() {
-    super.initState();
-    _webViewController = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse('https://mern-demo-kkmv.onrender.com'));
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Screenshot(
-        controller: screenshotController,
-        child: WebViewWidget(controller: _webViewController),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.screenshot),
-        onPressed: () async {
-          // Capture the screenshot
-          final capturedImage = await screenshotController
-              .captureFromLongWidget(InheritedTheme.captureAll(
-                  context,
-                  Material(
-                    child: _buildList(),
-                  )));
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Screenshot(
+            controller: screenshotController,
+            child: _buildList(),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.screenshot),
+          onPressed: () async {
+            // Capture the screenshot
+            final capturedImage = await screenshotController
+                .captureFromLongWidget(InheritedTheme.captureAll(
+                    context,
+                    Material(
+                      child: _buildList(),
+                    )));
 
-          if (capturedImage != null) {
             _showCapturedImage(capturedImage);
-          }
-        },
+          },
+        ),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import printing
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
-  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
